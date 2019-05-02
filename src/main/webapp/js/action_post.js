5cf9bd4d3a77abb136eaff9053105a3b727a85b8
Aydos <aidos.jantleu@gmail.com>
2019-05-02 07:22:41 +0600

    Added function for delete post

--- src/main/webapp/js/action_post.js
+++ src/main/webapp/js/action_post.js
@@ -5,13 +5,15 @@ $(function () {
 });
 
 function deletePost(caller) {
-    var POST_ID = caller.getAttribute('id_post');
+    var POST_ID = caller.getAttribute('post_id');
 
     $.ajax({
         type: "POST",
         url: "deletePost?id=" + POST_ID,
         success: function () {
             console.log(ITEM_ID + ' ' + TYPE_CATEGORY);
+            alert("Удален пост с ID " + POST_ID);
+            location.reload();
         }
     });
 }
