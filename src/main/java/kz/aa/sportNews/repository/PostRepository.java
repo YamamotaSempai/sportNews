b45e3ea579e50661e3c5581f0dc34670901b6ba4
YamamotaSempai <aidos.jantleu@gmail.com>
2019-05-09 14:56:20 +0600

    Added search & fixed pagination & added folder for images

--- src/main/java/kz/aa/sportNews/repository/PostRepository.java
+++ src/main/java/kz/aa/sportNews/repository/PostRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.validation.constraints.NotEmpty;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(Pageable pageable);
 
     @Modifying
     @Query("delete from Post p where p.id = :id")
     void delete(@Param("id") Long id);
+
+    Page<Post> findByTitleLike(@NotEmpty(message = "*Пожалуйста введите название статьи") String title, Pageable pageable);
 }
--- src/main/java/kz/aa/sportNews/service/PostService.java
+++ src/main/java/kz/aa/sportNews/service/PostService.java
@@ -5,7 +5,11 @@ import kz.aa.sportNews.service.base.BaseService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import javax.validation.constraints.NotEmpty;
+
 public interface PostService extends BaseService<Post, Long> {
     Page<Post> findAll(Pageable pageable);
     void deletePost(Long id);
+    Page<Post> findByTitleLike(@NotEmpty(message = "*Пожалуйста введите название статьи") String title, Pageable pageable);
+
 }
